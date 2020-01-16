cbed955c2cd67cb216b93a29967c1442f12aa1ce
gkundwa2-ai <59675047+gkundwa2-ai@users.noreply.github.com>
2020-01-16 16:19:52 -0600

    changes by the collaborator
    added my name and my major

--- hello/hello.c
+++ hello/hello.c
@@ -9,5 +9,8 @@
 int main(void){
 printf("Arnaaz Brar\n");// Statement to print the name
   printf("Electrical Engineering\n"); //Statement to print the major
+  Printf("My name is Gabin Kundwa");
+  printf("My major is Integrated Science");
+  //changes from the collaborator
 return 0;
 }
