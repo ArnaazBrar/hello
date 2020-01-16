4d69d12df604bc38ac58a8d59077d74ff8735a00
gkundwa2-ai <59675047+gkundwa2-ai@users.noreply.github.com>
2020-01-16 15:47:11 -0600

    Update hello.c
    added to my name and my major

--- hello/hello.c
+++ hello/hello.c
@@ -5,5 +5,8 @@
 int main(void){
 printf("Arnaaz Brar\n");
   printf("Electrical Engineering\n");
+    printf("My name is Gabin Kundwa");
+    printf("My major is Integrated Science");
+    /*collaborator Gabin Kundwa*/
 return 0;
 }
